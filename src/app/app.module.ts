311a1aed27d56df6a05e09a9cd640a903b8485e7
NikYanki <nankovoj799@gmail.com>
2022-05-15 12:06:04 +0200

    main master branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import {HttpClientModule} from "@angular/common/http";
-import { UserComponent } from './components/user/user.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { CommetsComponent } from './components/commets/commets.component';
-import { PostComponent } from './components/post/post.component';
-import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    PostsComponent,
-    CommetsComponent,
-    PostComponent,
-    CommentComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
